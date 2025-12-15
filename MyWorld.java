15d0a655640f97928f3a82829d67d6d84ab3195a
c4ldrn <348808122@gapps.yrdsb.ca>
2025-12-15 14:20:23 -0500

    addng score borad and apple

--- MyWorld.java
+++ MyWorld.java
@@ -6,5 +6,24 @@ public class MyWorld extends World {
         
         snake snake = new snake();
         addObject(snake, 300, 200);
+        
+                scoreBoard = new ScoreBoard();
+        addObject(scoreBoard, 70, 20);
+        
+        spawnApple();
+
+    }
+          public void spawnApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(getWidth() - 40) + 20;
+        int y = Greenfoot.getRandomNumber(getHeight() - 40) + 20;
+        addObject(apple, x, y);
+    
+    }
+    
+     public void addScore(int amount)
+    {
+        scoreBoard.add(amount);
     }
 }
