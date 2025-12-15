31a5e656abf65060974fafc6f15c0e4223be5e13
c4ldrn <348808122@gapps.yrdsb.ca>
2025-12-15 14:07:50 -0500

    adding snake

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        snake snake = new snake();
+        addObject(snake, 300, 200);
     }
 }
