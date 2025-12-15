9013ad8d99345e6cc8826b5be09d7076cb4036c2
c4ldrn <348808122@gapps.yrdsb.ca>
2025-12-15 14:27:15 -0500

    label and apple

--- MyWorld.java
+++ MyWorld.java
@@ -1,29 +1,39 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
-    public MyWorld() {
-        super(600, 400, 1);
-        
-        snake snake = new snake();
-        addObject(snake, 300, 200);
-        
-                scoreBoard = new ScoreBoard();
-        addObject(scoreBoard, 70, 20);
-        
-        spawnApple();
+public class MyWorld extends World
+{
+    private Label scoreLabel;
 
+    public MyWorld()
+    {    
+        super(600, 400, 1);
+
+        snake snake = new snake();
+        addObject(snake, getWidth()/2, getHeight()/2);
+
+        scoreLabel = new Label();
+        addObject(scoreLabel, 70, 20);
+
+        spawnApple();
     }
-          public void spawnApple()
+
+    public void spawnApple()
     {
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(getWidth() - 40) + 20;
         int y = Greenfoot.getRandomNumber(getHeight() - 40) + 20;
         addObject(apple, x, y);
-    
+    }
+
+    public void addScore(int amount)
+    {
+        scoreLabelact(amount);
     }
     
-     public void addScore(int amount)
+     public void gameOver()
     {
-        scoreBoard.add(amount);
+        showText("GAME OVER", getWidth()/2, getHeight()/2);
+        Greenfoot.stop();
     }
 }
+
