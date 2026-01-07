83aa4fa4dbfb560dabe1b54930b10719b18d1c59
c4ldrn <348808122@gapps.yrdsb.ca>
2026-01-07 13:57:56 -0500

    Update snake.java

--- snake.java
+++ snake.java
@@ -12,6 +12,8 @@ public class snake extends Actor
      * Act - do whatever the snake wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    // test
   
 
     public void act()
